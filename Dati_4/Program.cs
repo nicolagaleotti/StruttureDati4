08f601d11b9fd6b319f8a0dcc7488fda2604b3ca
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-05 11:37:32 +0100

    fine

--- Dati_4/Program.cs
+++ Dati_4/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Dati_4
 {
@@ -10,6 +11,32 @@ namespace Dati_4
     {
         static void Main(string[] args)
         {
+            string file = "test.txt";
+            Console.WriteLine("Inserisci il valore da cercare: ");
+            string v = Console.ReadLine();
+            if (File.Exists(file))
+            {
+                try
+                {
+                    using (StreamReader r = new StreamReader(file))
+                    {
+                        string line;
+                        while ((line = r.ReadLine()) != null)
+                        {
+                            if (line.Contains(v))
+                            {
+                                Console.WriteLine($"Il file contiene il valore {v}");
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
